bcf0550d4d26440ae3d6e8a13bc7cf27c56b8334
Ranudee Fernando <164500480+RanudeeFernando@users.noreply.github.com>
2024-12-06 14:28:57 +0530

    added further comments - user base controller

--- src/main/java/com/example/cm2601_cw_javafx/app/UserBaseController.java
+++ src/main/java/com/example/cm2601_cw_javafx/app/UserBaseController.java
@@ -3,22 +3,36 @@ package com.example.cm2601_cw_javafx.app;
 import com.example.cm2601_cw_javafx.model.User;
 import javafx.scene.control.Alert;
 
-public class BaseController {
+public class UserBaseController {
     private User user;
 
+    // Sets the current user
     public void setCurrentUser(User currentUser) {
         this.user = currentUser;
     }
 
+    // Retrieves the current user
     protected User getUser() {
         return user;
     }
 
+    // Shows an informational alert
     public void showAlert(String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setHeaderText(null);
         alert.setContentText(message);
         alert.showAndWait();
     }
+
+    // Shows an error alert
+    public void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.show();
+    }
+
+    // Clears input fields (to be overridden by subclasses)
+    public void clearFields(){}
 
 }
